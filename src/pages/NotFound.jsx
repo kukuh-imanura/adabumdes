3e6fc391f2e50082b3d83408b91752b7bce08bb6
kukuh-imanura <kukuh.imanura.bagaskara@gmail.com>
2024-02-05 21:15:13 +0800

    update Notfound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
 function Notfound() {
+  const [darkmode, setDarkmode] = useState(false);
+
+  useEffect(() => {
+    console.log(`Dark Mode : ${darkmode}`);
+    document.body.classList.toggle('dark');
+  }, [darkmode]);
+
   return (
-    <div className={`flex flex-col items-center justify-center h-screen bg-surface-primary dark:bg-dark-primary`}>
-      <h1 className='text-5xl cursor-pointer dark:text-light-primary' onClick={() => setDarkmode(!darkmode)}> 404 </h1>
+    <div
+      className={`flex flex-col items-center justify-center h-screen bg-surface-primary dark:bg-dark-primary`}
+    >
+      <h1
+        className='text-5xl cursor-pointer dark:text-light-primary'
+        onClick={() => setDarkmode(!darkmode)}
+      >
+        404
+      </h1>
       <h4 className='dark:text-light-primary'>Page Not Found</h4>
     </div>
   );
