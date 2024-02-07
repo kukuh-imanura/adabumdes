e9c37ef609f44f5ee4ed9f46ddd8c1fc675847f0
kukuh-imanura <kukuh.imanura.bagaskara@gmail.com>
2024-02-07 14:29:36 +0800

    end testing bootstrap-icons

--- src/component/Carousel.jsx
+++ src/component/Carousel.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Carousel(props) {
   return (
     <div className={`${props.class} h-75vh`}>
-      
+      <i className="bi bi-house"></i>
       Carousel
     </div>
   )
