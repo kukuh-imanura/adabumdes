c584cbdbe55eca0cbc06d70d8fde410c1678dc25
kukuh-imanura <kukuh.imanura.bagaskara@gmail.com>
2024-02-07 15:07:58 +0800

    end testing react-bootstrap-icons

--- src/component/Carousel.jsx
+++ src/component/Carousel.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { ChevronLeft, ChevronRight } from 'react-bootstrap-icons'
 
 function Carousel(props) {
   return (
     <div className={`${props.class} h-75vh`}>
-      <i className="bi bi-house"></i>
-      Carousel
+
+      {/* CONTENT */}
+      <div className={`bg-[${props.img}]`}>
+        <h1>{props.tittle}</h1>
+        <p>{props.desc}</p>
+      </div>
+
+      {/* BUTTON */}
+      <div className='px-10 h-full flex justify-between'>
+        <button>
+          <ChevronLeft size="32px" className='hover:accent-brand-primary'/>
+        </button>
+        <button>
+          <ChevronRight size="32px"/>
+        </button>
+      </div>
+
     </div>
   )
 }
